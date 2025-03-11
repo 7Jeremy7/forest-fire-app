7cdf97b1d760124811038c6c6b57d08f51ebe8a1
7Jeremy7 <116099030+7Jeremy7@users.noreply.github.com>
2025-03-11 18:08:11 -0500

    user

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -42,6 +42,7 @@ export async function POST(request: NextRequest){
             )
         }
 
+        //cambio de variable
         const {password: _userPass, ...rest} = userFind._doc;
 
         const token = jwt.sign({data: rest}, "secreto",{
