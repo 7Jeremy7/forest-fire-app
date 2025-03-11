af077cd35ce7d5b4face2b8c973e0123d5beb5f6
7Jeremy7 <116099030+7Jeremy7@users.noreply.github.com>
2025-03-11 17:58:32 -0500

    ignore

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -42,7 +42,7 @@ export async function POST(request: NextRequest){
             )
         }
 
-        const {password: userPass, ...rest} = userFind._doc;
+        const {password: _userPass, ...rest} = userFind._doc;
 
         const token = jwt.sign({data: rest}, "secreto",{
             expiresIn: 86400,
